bdab6bfac344312508d9102162c4fc26a6926f37
Alex <havemercyerasus@hotmail.com>
2014-02-04 12:08:05 +0100

    A journey of a thousand miles begins with a single step.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Ni::Application.config.secret_key_base = 'ec60031a9bcb8f3bcff2dd01d7492580657bdd4aca8ae74ebd707d7ae4127856cc68ab3ee5eb7aa2acacbee77e2d11cf4150183660b71b65e66c1c227c5d8562'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Ni::Application.config.session_store :cookie_store, key: '_ni_session'
